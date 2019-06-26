b7d28aed751a64a950dae6caaca6ae14d9407d57
Protocolo OpenFlow <ernandopassosjr@gmail.com>
2019-06-26 00:28:51 -0300

    put code data set

--- data_set.py
+++ data_set.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import time
+import logging
+
 
 class DataSetReader(object):
 
@@ -11,47 +13,66 @@ class DataSetReader(object):
         self.lineHumid = 0
         self.lineLigh = 0
         self.lineVolt = 0
+        self.countLine = 0
+        logging.basicConfig(filename = 'app.log', level = logging.INFO)
         self.reader_data()
-
+	
+	#chunksize=100
+	#iterator=True
     def reader_data(self):
         #self.open_data = open(self.dir, 'rb')
-        self.open_data = pd.read_csv(self.dir, sep=" ", header=None)
-        self.countLine = self.open_data.shape
+        self.open_data = pd.read_csv(self.dir, sep=" ", header=None, chunksize=10)
+        #self.countLine = self.open_data.shape
+        print("Data Readear: open data sets")
 
     def next_value(self, sensor):    
         #line = self.open_data.readline()
         #splitLine = line.split(" ")
-        if(sensor=="temperature"):
-            if(self.lineTemp==self.countLine):
-                self.lineTemp = 0    
-            value = self.open_data.loc[self.lineTemp] 
-            value = value[4]
-            self.lineTemp = self.lineTemp+1
-        if(sensor=="humidity"):
-            if(self.lineHumid==self.countLine):
-                self.lineHumid = 0    
-            value = self.open_data.loc[self.lineHumid] 
-            value = value[5]
-            self.lineHumid = self.lineHumid+1            
-        if(sensor=="ligh"):
-            if(self.lineLigh==self.countLine):
+        #print("Data reader: next value")
+        if(sensor=="temperatureSensor"):
+            try:
+				if(self.lineTemp==10):
+					#self.reader_data()
+					self.lineTemp = 0  
+				
+				value = pd.DataFrame(self.open_data.get_chunk(1))
+				value = value.get_values()
+				value = value[0][4]
+				
+				self.lineTemp = self.lineTemp+1
+				self.countLine = self.countLine+1
+				#logging.exception("line "+ str(self.countLine))
+            except Exception as inst:
+				print(inst)
+				logging.exception(str(inst))
+        if(sensor=="humiditySensor"):
+            if(self.lineHumid==2313155):
+				self.lineHumid = 0    
+				
+				value = pd.DataFrame(self.open_data.get_chunk(1))
+				value = value.get_values()
+				value = value[0][4]
+				
+				self.lineHumid = self.lineHumid+1
+        if(sensor=="lighSensor"):
+            if(self.lineLigh==self.countLine[0]-1):
                 self.lineLigh = 0 
             value = self.open_data.loc[self.lineLigh] 
             value = value[6]
             self.lineLigh = self.lineLigh+1 
-        if(sensor=="voltage"):
-            if(self.lineVolt==self.countLine):
+        if(sensor=="voltageSensor"):
+            if(self.lineVolt==self.countLine[0]-1):
                 self.lineVolt = 0 
             value = self.open_data.loc[self.lineVolt] 
             value = value[7]
             self.lineVolt = self.lineVolt+1 
 
-        print(value)
+        #print(value)
         return value
 
-#dataSet = DataSetReader("/media/brenno/Brenno/DataSet-intel/dataset-intel/data.txt")
+#dataSet = DataSetReader("/home/openflow/FoT-Simulation/dataset/dataSet_temp.txt")
 
 #thread = True
 #while thread == True:
-#    dataSet.next_value("temperature")
-#    time.sleep(5)
+	#dataSet.next_value("temperatureSensor")
+    #time.sleep(5)
